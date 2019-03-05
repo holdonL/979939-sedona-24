aaccd300bf3c3608e25d70692b34419d965e2e5e
holdonL <pavelshets@gmail.com>
2019-03-06 00:00:45 +0300

    поправил по основным критериям

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,8 @@
 var modal = document.querySelector(".modal");
 var button = document.querySelector(".button-search");
 
+modal.classList.add("js");
+
 button.addEventListener("click", toggleMenu);
 
 function toggleMenu(evt) {
